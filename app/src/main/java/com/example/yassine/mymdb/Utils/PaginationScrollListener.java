508840fbf09da2a9ebd8742f47c62d705c1b47cf
cheqri <cheqriyass@gmail.com>
2017-11-27 16:43:11 +0100

    did some code cleaning v.0.9.7

--- app/src/main/java/com/example/yassine/mymdb/Utils/PaginationScrollListener.java
+++ app/src/main/java/com/example/yassine/mymdb/Utils/PaginationScrollListener.java
@@ -3,11 +3,7 @@ package com.example.yassine.mymdb.Utils;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-/**
- * Pagination
- * Created by Suleiman19 on 10/15/16.
- * Copyright (c) 2016. Suleiman Ali Shakir. All rights reserved.
- */
+
 public abstract class PaginationScrollListener extends RecyclerView.OnScrollListener {
 
     LinearLayoutManager layoutManager;
